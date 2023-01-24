7c6c4a1a5237924327b6dd187f4da005a04b0e93
ssilman <steve.silman@wundermanthompson.com>
2023-01-24 14:50:23 +0000

    ssh key from aws

--- modules/compute.tf
+++ modules/compute.tf
@@ -1,5 +1,7 @@
-data "local_file" "public_ssh_key" {
-  filename = "/Users/ssilman/.ssh/id_rsa.pub"
+
+
+data "aws_secretsmanager_secret" "public_ssh_key_from_aws" {
+  name = "MY_MAC_SSH_PUB"
 }
 
 resource "aws_instance" "ec2-instance" {
@@ -8,7 +10,7 @@ resource "aws_instance" "ec2-instance" {
   security_groups = [aws_security_group.ec2-instances.name]
   user_data       = <<-EOF
                     #!/bin/bash
-                    sudo -u ubuntu bash -c 'echo "${data.local_file.public_ssh_key.content}" >> ~/.ssh/authorized_keys'
+                    sudo -u ubuntu bash -c 'echo "${data.aws_secretsmanager_secret.public_ssh_key_from_aws.content}" >> ~/.ssh/authorized_keys'
                     sudo -u ubuntu bash -c 'echo wanker-ssilman  > /tmp/wanker.txt'
               EOF
 }
