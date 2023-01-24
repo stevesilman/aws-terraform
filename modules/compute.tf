df7518d311b0d492941e846e68542831d52bcebd
ssilman <steve.silman@wundermanthompson.com>
2023-01-24 21:34:19 +0000

    change2

--- modules/compute.tf
+++ modules/compute.tf
@@ -12,6 +12,10 @@ output "sensitive_ssh_hash" {
   value = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.current.secret_string))
 }
 
+data "local_file" "public_ssh_key" {
+  filename = "/Users/ssilman/.ssh/id_rsa.pub"
+}
+
 resource "aws_instance" "ec2-instance" {
   ami             = var.ami
   instance_type   = var.instance_type
@@ -19,6 +23,7 @@ resource "aws_instance" "ec2-instance" {
   user_data       = <<-EOF
                     #!/bin/bash
                     sudo -u ubuntu bash -c 'echo "${data.aws_secretsmanager_secret_version.current.secret_string}" >> ~/.ssh/authorized_keys'
+                    sudo -u ubuntu bash -c 'echo "${data.local_file.public_ssh_key.content}" >> ~/.ssh/authorized_keys'
                     sudo -u ubuntu bash -c 'echo wanker-ssilman  > /tmp/wanker.txt'
               EOF
 }
