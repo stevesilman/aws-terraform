70f7e945aa8f4180fa2fd655cc9dc04017b101ab
ssilman <steve.silman@wundermanthompson.com>
2023-01-25 13:33:05 +0000

    try a fix

--- modules/compute.tf
+++ modules/compute.tf
@@ -8,16 +8,13 @@ data "aws_secretsmanager_secret_version" "current" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
-data "local_file" "public_ssh_key" {
-  filename = "/Users/ssilman/.ssh/id_rsa.pub"
-}
 resource "aws_instance" "ec2-instance" {
   ami             = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.ec2-instances.name]
   user_data       = <<-EOF
                     #!/bin/bash
-                    sudo -u ubuntu bash -c 'echo "${data.local_file.public_ssh_key.content}" >> ~/.ssh/authorized_keys'
+                    #sudo -u ubuntu bash -c 'echo "${data.local_file.public_ssh_key.content}" >> ~/.ssh/authorized_keys'
                     sudo -u ubuntu bash -c 'echo "${data.aws_secretsmanager_secret_version.current.secret_string}" >> ~/.ssh/authorized_keys'
                  EOF
 }
