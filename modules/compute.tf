ae4e39f2693ce7cef228f1746bf8fd14e93a093b
ssilman <steve.silman@wundermanthompson.com>
2023-01-26 15:52:28 +0000

    new ec2 config 3

--- modules/compute.tf
+++ modules/compute.tf
@@ -31,10 +31,6 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.generated_key.key_name
-
-  tags {
-    Name = "HelloWorld"
-  }
 }
 
 output "private_key" {
