71dd5a48f64fef923e35e50af72a9b269d70086a
ssilman <steve.silman@wundermanthompson.com>
2023-01-26 12:57:25 +0000

    output added

--- modules/compute.tf
+++ modules/compute.tf
@@ -8,7 +8,7 @@ data "aws_secretsmanager_secret_version" "current" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
-output "secrets" {
+output "ssh_pub_key" {
   value = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["MY_MAC_SSH_PUB_KEY"]
 }
 
@@ -18,7 +18,8 @@ resource "aws_instance" "ec2-instance" {
   security_groups = [aws_security_group.ec2-instances.name]
   user_data       = <<-EOF
                     #!/bin/bash
-                    sudo -u ubuntu bash -c 'echo "${data.aws_secretsmanager_secret_version.current.secret_string}" >> ~/.ssh/authorized_keys'
+                    sudo -u ubuntu bash -c 'echo "${value}" >> ~/.ssh/authorized_keys'
                  EOF
 }
 
+#data.aws_secretsmanager_secret_version.current.secret_string
