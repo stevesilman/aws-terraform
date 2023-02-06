68488c759ab842d65dd30eccfabd6cd705f80133
ssilman <steve.silman@wundermanthompson.com>
2023-02-06 21:18:09 +0000

    here we go again5

--- modules/compute.tf
+++ modules/compute.tf
@@ -31,7 +31,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name         = aws_key_pair.generated_key.key_name
+  key_name         = aws_key_pair.generated_key.key_name 
+  tags {
+    Name = "HelloWorld"
+  }
 }
 
 output "private_key" {
