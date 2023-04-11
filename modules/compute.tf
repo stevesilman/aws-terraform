0dd397173b284158e13a7fa5fa31a8ac427deaf0
stevesilman <steve.silman@ntlworld.com>
2023-04-11 15:11:57 +0100

    Update compute.tf

--- modules/compute.tf
+++ modules/compute.tf
@@ -1,33 +1,3 @@
-data "aws_key_pair" "example" {
-  key_name           = "myseckeymac"
-  include_public_key = true
-
-  filter {
-    name   = "tag:Component"
-    values = ["web"]
-  }
-}
-
-output "fingerprint" {
-  value = data.aws_key_pair.example.fingerprint
-}
-
-output "name" {
-  value = data.aws_key_pair.example.key_name
-}
-
-output "id" {
-  value = data.aws_key_pair.example.id
-}
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = var.key_name
-  public_key = tls_private_key.example.public_key_openssh
-}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -49,7 +19,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = "myseckeymac"
 }
 
 
